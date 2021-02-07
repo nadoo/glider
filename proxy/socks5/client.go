d2268b623f435f9bee58ecfe08716a4e3e0a8c0a
mzz <m@mzz.pub>
2021-02-07 11:04:58 +0800

    fix(socks5): should not dial returned bind addr directly (#212)
    * fix(socks5): should not dial returned bind addr directly
    When server returns an any ip (0.0.0.0 or [::0]), we should use conventional ip to replace the any ip given (0.0.0.0 or [::0]).
    This behaviour adapts to most situations.
    
    See v2fly/v2ray-core#523
    
    * fix: splithostport

--- proxy/socks5/client.go
+++ proxy/socks5/client.go
@@ -86,9 +86,20 @@ func (s *Socks5) DialUDP(network, addr string) (pc net.PacketConn, writeTo net.A
 		return nil, nil, err
 	}
 
-	pc, nextHop, err := s.dialer.DialUDP(network, uAddr.String())
+	var uAddress string
+	h, p, _ := net.SplitHostPort(uAddr.String())
+	// if returned bind ip is unspecified
+	if ip := net.ParseIP(h); ip != nil && ip.IsUnspecified() {
+		// indicate using conventional addr
+		h, _, _ = net.SplitHostPort(s.addr)
+		uAddress = net.JoinHostPort(h, p)
+	} else {
+		uAddress = uAddr.String()
+	}
+
+	pc, nextHop, err := s.dialer.DialUDP(network, uAddress)
 	if err != nil {
-		log.F("[socks5] dialudp to %s error: %s", uAddr.String(), err)
+		log.F("[socks5] dialudp to %s error: %s", uAddress, err)
 		return nil, nil, err
 	}
 
